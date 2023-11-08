e2cfc90583653d7c6aa3125635ffe3d1f99f36b8
Mohamad Bashir Aljounaidy <bma.963@gmail.com>
2023-11-08 13:35:28 +0400

    prometheus added , json api added

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const os = require('os');
 const osUtils = require('os-utils');
-const path = require('path');
-app.use(express.static('public'));
-
-const interfaces = os.networkInterfaces();
-const hostIP = interfaces.eth0[0].address;
+const promBundle = require("express-prom-bundle");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const metricsMiddleware = promBundle({ includeMethod: true, includePath: true });
+
+app.use(metricsMiddleware);
+app.use(express.static('public'));
+
+const interfaces = os.networkInterfaces();
+const hostIP = interfaces.eth0 ? interfaces.eth0[0].address : 'Not Available';
 
 app.get('/', (req, res) => {
   const osName = os.type();
@@ -22,6 +26,30 @@ app.get('/', (req, res) => {
     res.set('Content-Type', 'text/plain');
     res.send(responseText);
   });
+});
+
+app.get('/api', (req, res) => {
+  const osName = os.type();
+  const hostName = os.hostname();
+  
+  osUtils.cpuUsage(function(cpuUsage) {
+    const currentLoad = (cpuUsage * 100).toFixed(2);
+    
+    const response = {
+      osName: osName,
+      hostIP: hostIP,
+      hostName: hostName,
+      currentLoad: `${currentLoad}%`,
+      version: '1.2.0'
+    };
+    
+    res.json(response);
+  });
+});
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', metricsMiddleware.Prometheus.register.contentType);
+  res.end(metricsMiddleware.Prometheus.register.metrics());
 });
 
 app.listen(PORT, () => {
